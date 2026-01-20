74ebb346866c15f1180706f8d18f400b19e02132
Christina Nissen <christinaf.nissen@hotmail.com>
2026-01-20 11:43:01 +0100

    Update project based on Rosario & Oksana's feedback

--- src/Components/NoPrivateMode.js
+++ src/Components/NoPrivateMode.js
@@ -31,7 +31,8 @@ const NoPrivateMode = () => {
 
         <div className="study-modal study-modal-static">
           <p>
-            For the purposes of this study, please do not use private browsing or open the login in a new window or tab.<br /><br />
+            For the purposes of this study, please do not use private browsing or open the login in a new window or tab.
+            Otherwise, your participation may not be correctly registered on Prolific.<br /><br />
             To ensure your interaction is properly registered, simply click the login button below to proceed.
           </p>
           <div className="study-modal-actions">
